a6a915225fdbf5ff5605eab1bf646566734d899b
Tejaswa <tejaswahinduja@gmail.com>
2025-05-20 02:10:56 +0530

    Changed the base url

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -4,7 +4,7 @@
 const config = {
     // API endpoints
     API: {
-      BASE_URL: 'http://localhost:5000',
+      BASE_URL: import.meta.env.VITE_API_BASE_URL,
       ENDPOINTS: {
         TWITTER_AUTH: '/api/auth/twitter',
         TWITTER_CALLBACK: '/api/auth/twitter/callback',
